5813bef195c08acc09da14013643e90b59de9015
yjj <yoojj.xyz@gmail.com>
2021-01-31 02:58:44 +0900

    init

---
+++ blog/react/src/components/Logo.js
@@ -0,0 +1,9 @@
+const Logo = () => {
+    return(
+        <a href="" title="포스타입 로고" className="common-logo">
+            <span className="hide">포스타입</span>
+        </a>
+    );
+}
+
+export default Logo;
